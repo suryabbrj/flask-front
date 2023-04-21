3568a2630821c7a0bc1be909fa5b08ae85a4abe2
unknown <suryababuraj02@gmail.com>
2023-04-21 12:03:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+
+from gradio_client import Client
+
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/after', methods=['GET', 'POST'])
+def after():
+    file = request.files['file1']
+    file.save('static/file.jpg')
+
+    client = Client("https://suryabbrj-collegeprojectv2.hf.space/")
+
+    result = client.predict('static/file.jpg', api_name="/predict")
+    print(result)
+    return render_template('predict.html', result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
